7438f80e02b945f98ec7eb2740f325072375c6e1
Antonio Dottori <aadottori@gmail.com>
2021-05-21 02:44:51 -0300

    Add relationships and foreign keys

--- blog/main.py
+++ blog/main.py
@@ -28,7 +28,7 @@ def all(db: Session = Depends(get_db)):
 
 @app.post("/blog", status_code=status.HTTP_201_CREATED, response_model = List[schemas.ShowBlog], tags = ["blogs"])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
