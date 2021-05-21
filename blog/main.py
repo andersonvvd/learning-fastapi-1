74e9d290f7bddbbcf1c959edb4096347b1f920ee
Antonio Dottori <aadottori@gmail.com>
2021-05-21 02:09:43 -0300

    Documentation Tags

--- blog/main.py
+++ blog/main.py
@@ -20,7 +20,13 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED, response_model = List[schemas.ShowBlog])
+@app.get("/blog", tags = ["blogs"])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED, response_model = List[schemas.ShowBlog], tags = ["blogs"])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -29,13 +35,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-
-@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags = ["blogs"])
 def show_single(id, response:Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -43,7 +43,7 @@ def show_single(id, response:Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags = ["blogs"])
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -53,7 +53,7 @@ def destroy(id, db: Session = Depends(get_db)):
     return "Done"
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED) 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags = ["blogs"]) 
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -63,12 +63,10 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return "Update"
 
 
-
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@app.post("/user", response_model=schemas.ShowUser)
+@app.post("/user", response_model=schemas.ShowUser, tags = ["users"])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-
     new_user = models.User(name = request.name, email = request.email, password = hashing.Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
@@ -76,7 +74,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/user/{id}", response_model = schemas.ShowUser)
+@app.get("/user/{id}", response_model = schemas.ShowUser, tags = ["users"])
 def get_user(id:int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
