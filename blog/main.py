eab50a8cf715bba1d4cebf8e260730a41efab509
Antonio Dottori <aadottori@gmail.com>
2021-05-21 02:05:55 -0300

    User, Hashing - GET and POST

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from pydantic import BaseModel
-import schemas, models
+import schemas, models, hashing
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-
+from typing import List
+from passlib.context import CryptContext
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
+@app.post("/blog", status_code=status.HTTP_201_CREATED, response_model = List[schemas.ShowBlog])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -34,7 +35,7 @@ def all(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show_single(id, response:Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -60,3 +61,24 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog.update(request.dict()) 
     db.commit() 
     return "Update"
+
+
+
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post("/user", response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+
+    new_user = models.User(name = request.name, email = request.email, password = hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user/{id}", response_model = schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not available.")
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -3,5 +3,23 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
-    class Config:
+
+
+class ShowBlog(Blog):
+    title: str
+    body: str
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    class Config():
         orm_mode = True
